9748f7f13d3f4ffd1883e26c2a67cb287ed114c2
Andrei Tanana <atanana@ampsoft.by>
2016-12-02 16:29:23 +0300

    real work init

--- app/com/atanana/DuelModule.scala
+++ app/com/atanana/DuelModule.scala
@@ -5,6 +5,7 @@ import javax.inject.{Named, Singleton}
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.routing.RoundRobinPool
 import com.atanana.actor.{DuelsProcessor, DuelsQueue}
+import com.atanana.parsers.TeamParser
 import com.google.inject.{AbstractModule, Provides}
 import play.api.libs.concurrent.AkkaGuiceSupport
 import play.api.libs.ws.WSClient
@@ -13,12 +14,15 @@ import play.api.libs.ws.WSClient
 class DuelModule extends AbstractModule with AkkaGuiceSupport {
   override def configure(): Unit = {
     bindActor[DuelsQueue]("DuelsQueue")
+
+    bind(classOf[TeamParser]).toInstance(new TeamParser)
   }
 
   @Provides
   @Singleton
   @Named("DuelsProcessorRouter")
-  def duelsProcessorRouter(actorSystem: ActorSystem, ws: WSClient): ActorRef = {
-    actorSystem.actorOf(RoundRobinPool(1).props(Props(new DuelsProcessor(ws))), "DuelsProcessorRouter")
+  def duelsProcessorRouter(actorSystem: ActorSystem, ws: WSClient, teamParser: TeamParser): ActorRef = {
+    val props = Props(new DuelsProcessor(ws, teamParser))
+    actorSystem.actorOf(RoundRobinPool(1).props(props), "DuelsProcessorRouter")
   }
 }
--- app/com/atanana/actor/DuelsProcessor.scala
+++ app/com/atanana/actor/DuelsProcessor.scala
@@ -3,13 +3,13 @@ package com.atanana.actor
 import javax.inject.Inject
 
 import akka.actor.Actor
+import com.atanana.parsers.TeamParser
 import play.api.libs.ws.WSClient
 
 import scala.util.{Failure, Success}
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class DuelsProcessor @Inject()(ws: WSClient) extends Actor {
+class DuelsProcessor @Inject()(ws: WSClient, teamParser: TeamParser) extends Actor {
   val TEAM_ADDRESS = "http://rating.chgk.info/team/"
 
   override def receive: Receive = {
@@ -18,6 +18,8 @@ class DuelsProcessor @Inject()(ws: WSClient) extends Actor {
         .zip(teamPage(duelRequest.teamId2))
         .onComplete {
           case Success((teamPage1, teamPage2)) =>
+            val tournaments = teamParser.parse(teamPage1).toSet.intersect(teamParser.parse(teamPage2).toSet)
+            duelRequest.listener ! DuelResult(tournaments.toString(), duelRequest.uuid)
           case Failure(exception) => //todo show error
         }
   }
