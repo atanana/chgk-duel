6468b94d48a0648e154a156d5b84e310351cb4ec
Andrei Tanana <atanana@ampsoft.by>
2016-12-13 16:41:54 +0300

    add gzip

---
+++ app/com/atanana/Filters.scala
@@ -0,0 +1,8 @@
+package com.atanana
+
+import javax.inject.Inject
+
+import play.api.http.DefaultHttpFilters
+import play.filters.gzip.GzipFilter
+
+class Filters @Inject()(gzipFilter: GzipFilter) extends DefaultHttpFilters(gzipFilter)
--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
+  filters,
   specs2 % Test,
   "net.ruippeixotog" %% "scala-scraper" % "1.1.0",
   "com.typesafe.akka" % "akka-testkit_2.11" % "2.4.14" % "test",
